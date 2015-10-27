4368dcfe51c64a532d4be82e4b16f79fe60c0774
Greg McNutt <gcmcnutt@yahoo.com>
2015-10-26 22:01:43 -0700

    -- some grunt prep -- demo vis.js...

--- sensorWeb/Gruntfile.js
+++ sensorWeb/Gruntfile.js
@@ -6,10 +6,46 @@ grunt.initConfig({
         example: {
             port: 9000
         }
+    },
+    uglify: {
+        options: {
+            banner: '/*! <%= pkg.name %> <%= grunt.template.today("yyyy-mm-dd") %> */\n'
+        },
+        build: {
+            src: 'src/<%= pkg.name %>.js',
+            dest: 'build/<%= pkg.name %>.min.js'
+        }
+    },
+    concat: {
+        options: {
+            // define a string to put between each file in the concatenated output
+            separator: ';'
+        },
+        dist: {
+            // the files to concatenate
+            src: ['src/*.js'],
+            // the location of the resulting JS file
+            dest: 'dist/<%= pkg.name %>.js'
+        }
+    },
+    watch: {
+        scripts: {
+            files: ['**/*.js'],
+            tasks: ['concat'],
+            options: {
+                spawn: true,
+            },
+        },
     }
 });
     
 grunt.loadNpmTasks('grunt-connect');
-grunt.registerTask('default', 'connect:example');
+grunt.loadNpmTasks('grunt-contrib-uglify');
+grunt.loadNpmTasks('grunt-contrib-concat');
+grunt.loadNpmTasks('grunt-contrib-watch');
+
+grunt.registerTask('serve', ['connect:example']);
+grunt.registerTask('package', 'concat');
+
     
 };
